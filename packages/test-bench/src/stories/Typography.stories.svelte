3150708f7b3d6f895672c966d4b048bc2e306e21
Bruno Gregório <accounts@bruno.pw>
2026-02-11 16:40:53 -0300

    feat: added headings to storybook.

---
+++ packages/test-bench/src/stories/Typography.stories.svelte
@@ -0,0 +1,17 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf'
+
+  const { Story } = defineMeta({
+    title: 'Typography'
+  })
+</script>
+
+<Story name="Headings">
+  <h1 class="ow-display-heading">Display Heading</h1>
+  <p class="ow-heading-1">Heading 1</p>
+  <p class="ow-heading-2">Heading 2</p>
+  <p class="ow-heading-3">Heading 3</p>
+  <p class="ow-heading-4">Heading 4</p>
+  <p class="ow-heading-5">Heading 5</p>
+  <p class="ow-heading-6">Heading 6</p>
+</Story>
