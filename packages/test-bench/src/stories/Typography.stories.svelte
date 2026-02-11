2be857e564ee1e2f135dbdea33d777e46ec560b5
Bruno Gregório <accounts@bruno.pw>
2026-02-11 17:09:12 -0300

    fix: headings look right now.

--- packages/test-bench/src/stories/Typography.stories.svelte
+++ packages/test-bench/src/stories/Typography.stories.svelte
@@ -8,10 +8,13 @@
 
 <Story name="Headings">
   <h1 class="ow-display-heading">Display Heading</h1>
+  <br />
   <p class="ow-heading-1">Heading 1</p>
   <p class="ow-heading-2">Heading 2</p>
+  <br />
   <p class="ow-heading-3">Heading 3</p>
   <p class="ow-heading-4">Heading 4</p>
+  <br />
   <p class="ow-heading-5">Heading 5</p>
   <p class="ow-heading-6">Heading 6</p>
 </Story>
