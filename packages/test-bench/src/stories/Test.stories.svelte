05579d5634e844412cdc5b4d3433035d2cc093fb
Bruno Gregório <accounts@bruno.pw>
2026-02-11 14:37:26 -0300

    refactor: removed some files.

---
+++ packages/test-bench/src/stories/Test.stories.svelte
@@ -0,0 +1,11 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf'
+
+  const { Story } = defineMeta({
+    title: 'Test'
+  })
+</script>
+
+<Story name="Test">
+  <div>Test</div>
+</Story>
